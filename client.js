16378eb41fe0992ecbaef7d4213791431983eccb
spicecat <ateh0701@gmail.com>
2022-12-05 10:39:55 -0600

    testing getting correct slash command reply

--- client.js
+++ client.js
@@ -36,7 +36,7 @@ const getChannelCommands = async ({ channel, commandFields = COMMAND_FIELDS, opt
 const getSlashReply = async ({ channelId, targetId, commandName, ...args }) => {
     const channel = await getChannel({ channelId, targetId });
     const slash = await sendSlash({ channel, commandName, args });
-    return getReply(slash);
+    return getReply(slash, channel);
 }
 
 const sendSlash = async ({ channel, commandName, args }) => {
@@ -47,48 +47,54 @@ const sendSlash = async ({ channel, commandName, args }) => {
 }
 
 
-const getReply = async ({ id }) => {
-    const replyListenerCallback = resolve => reply => {
-        if (reply.reactions.message.interaction?.id === id) {
-            // console.log(49183, reply, this)
-            const message = pick(reply.reactions.message, MESSAGE_FIELDS);
-            message.embeds = pickArr(message.embeds, EMBED_FIELDS);
-            resolve(message);
-        };
+const getReply = async (slash, channel) => {
+    // const replyListenerCallback = resolve => reply => {
+    //     if (reply.nonce === nonce) {
+    //         console.log(49183, reply)
+    //         const message = pick(reply.reactions.message, MESSAGE_FIELDS);
+    //         message.embeds = pickArr(message.embeds, EMBED_FIELDS);
+    //         // resolve(message);
+    //     };
+    // }
+
+    // const replyListener = event => {
+    //     let callback;
+    //     const listener = new Promise(resolve => client.on(event, callback = replyListenerCallback(resolve)))
+    //     return { callback, listener }
+    // };
+
+    // const messageCreate = replyListener('messageCreate');
+    // const messageUpdate = replyListener('messageUpdate');
+
+    // const res = await Promise.race([
+    //     messageCreate.listener,
+    //     // messageUpdate.listener,
+    //     new Promise(resolve => setTimeout(resolve, TIMEOUT))
+    // ]);
+
+    console.log(93876142, slash.nonce, )
+    const filter = msg => {
+        console.log(102, msg.nonce === slash.nonce, msg.reactions, msg)
+        return 1
     }
 
-    const replyListener = event => {
-        let callback;
-        const listener = new Promise(resolve => client.on(event, callback = replyListenerCallback(resolve)))
-        return { callback, listener }
-    };
-
-    const messageCreate = replyListener('messageCreate');
-    const messageUpdate = replyListener('messageUpdate');
-
-    const res = await Promise.race([
-        messageCreate.listener,
-        messageUpdate.listener,
-        new Promise(resolve => setTimeout(resolve, TIMEOUT))
-    ]);
-
-    client.removeListener('messageCreate', messageCreate.callback);
-    client.removeListener('messageUpdate', messageUpdate.callback);
-
-    return res;
+    return new Promise(resolve => {
+        channel.awaitMessages({ filter, max: 2, time: TIMEOUT, errors: ['time'] })
+            .then(collected => {
+                resolve(collected)
+                // console.log(84576, collected)
+            })
+            .catch(collected => {
+                console.log(4357, collected)
+                resolve(collected)
+            });
+    })
 }
 
 client.once('ready', async () => {
     console.log('Logged in as:', client.user.username);
-})
-
-// const retry = async (cmd, count = RETRY_LIMIT) => {
-//     const ret = await cmd();
-//     return ret || !count ? ret : retry(cmd, count - 1);
-// };
+});
 
 client.login(TOKEN);
-// client.on('messageCreate', z => console.log('messageCreate', z));
-// client.on('messageUpdate', z => console.log('messageUpdate', z));
 
 module.exports = { getCommands, getSlashReply };
